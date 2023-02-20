72b2c9f856b4e008000bea7627f0cd5d8b159ca5
Anastasiia <anastasia.alexeenko@gmail.com>
2023-02-20 11:19:10 +0100

    💥 toggle animation

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,13 +1,18 @@
 import React from "react"
 
 export default function Toggle () {
+
+    function onToggle () {
+        document.getElementById("toggle__ball").classList.toggle("active");
+    }
+
     return (
         <div className="toggle-wrapper">
             <h2>Our pricing</h2>
             <div className="toggle">
                 <p className="toggle__p">Annually</p>
-                <div className="toggle__box">
-                    <div className="toggle__ball"></div>
+                <div className="toggle__box" onClick={onToggle}>
+                    <div className="toggle__ball" id="toggle__ball"></div>
                 </div>
                 <p className="toggle__p">Monthly</p>
             </div>
